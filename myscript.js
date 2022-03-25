ff4c4d34a51623c31f122ce49ee9e931a3ab6cf3
fezafatima004 <86085652+fezafatima004@users.noreply.github.com>
2022-03-25 15:30:51 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,60 @@
+function myFunction1(){
+    var period = prompt("please enter the subject name:");
+    document.getElementById("m-1period").innerHTML = period;
+}
+function myFunction2(){
+    var period = prompt("please enter the subject name:");
+    document.getElementById("t-1period").innerHTML = period;
+}
+function myFunction3(){
+    var period = prompt("please enter the subject name:");
+    document.getElementById("w-1period").innerHTML = period;
+}
+function myFunction4(){
+    var period = prompt("please enter the subject name:");
+    document.getElementById("th-1period").innerHTML = period;
+}
+function myFunction5(){
+    var period = prompt("please enter the subject name:");
+    document.getElementById("f-1period").innerHTML = period;
+}
+function myFunction21(){
+    var period = prompt("please enter the subject name:");
+    document.getElementById("m-2period").innerHTML = period;
+}
+function myFunction22(){
+    var period = prompt("please enter the subject name:");
+    document.getElementById("t-2period").innerHTML = period;
+}
+function myFunction23(){
+    var period = prompt("please enter the subject name:");
+    document.getElementById("w-2period").innerHTML = period;
+}
+function myFunction24(){
+    var period = prompt("please enter the subject name:");
+    document.getElementById("th-2period").innerHTML = period;
+}
+function myFunction25(){
+    var period = prompt("please enter the subject name:");
+    document.getElementById("f-2period").innerHTML = period;
+}
+function myFunction31(){
+    var period = prompt("please enter the subject name:");
+    document.getElementById("m-3period").innerHTML = period;
+}
+function myFunction32(){
+    var period = prompt("please enter the subject name:");
+    document.getElementById("t-3period").innerHTML = period;
+}
+function myFunction33(){
+    var period = prompt("please enter the subject name:");
+    document.getElementById("w-3period").innerHTML = period;
+}
+function myFunction34(){
+    var period = prompt("please enter the subject name:");
+    document.getElementById("th-3period").innerHTML = period;
+}
+function myFunction35(){
+    var period = prompt("please enter the subject name:");
+    document.getElementById("f-3period").innerHTML = period;
+}
